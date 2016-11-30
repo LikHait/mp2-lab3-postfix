125bd3776e557fa12cb0107bec6641af0a90c25d
LikHait <hait.2050@gmail.com>
2016-11-30 15:27:21 +0300

    Добавлены тесты

--- Окунев/base_test/test_tpostfix.cpp
+++ Окунев/base_test/test_tpostfix.cpp
@@ -31,3 +31,40 @@ TEST(TPostfix, can_return_postfix)
     postfix = str;
     EXPECT_EQ(postfix.GetPostfix(), "a b c * + ");
 }
+
+TEST(TPostfix, can_calculate)
+{
+    string str = "3.14+65*12-36";
+    TPostfix postfix;
+    postfix = str;
+    EXPECT_EQ(postfix.Calculate(), 747.14);
+}
+
+TEST(TPostfix, can_not_use_invalid_combination)
+{
+    string str = "a+*7";
+    TPostfix postfix;
+    ASSERT_ANY_THROW(postfix = str);
+}
+
+TEST(TPostfix, can_not_use_invalid_combination_2)
+{
+    string str = "a+)7";
+    TPostfix postfix;
+    ASSERT_ANY_THROW(postfix = str);
+}
+
+TEST(TPostfix, can_not_use_invalid_combination_3)
+{
+    string str = "a+-7";
+    TPostfix postfix;
+    ASSERT_ANY_THROW(postfix = str);
+}
+
+TEST(TPostfix, can_calculate_function)
+{
+    string str = "sqrt(9+7)";
+    TPostfix postfix;
+    postfix = str;
+    EXPECT_EQ(postfix.Calculate(), 4);
+}
